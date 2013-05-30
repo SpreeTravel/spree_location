e49606d07c3511a5399b4699283d4d03d0eff425
snc <snc@jaf.com>
2013-05-30 17:06:26 -0500

    improve admin decotator

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -1,12 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_latitude_and_longitude",
-                     :insert_after => "[data-hook='admin_product_form_right']",
-                     :text => "
-						<p>Add Geographic coordinate</p>												
-					 "						
-					 )                                                               
-#				     <%= f.label :location.latitude, I18n.t('latitude') %> <br />
-#					 <%= f.label :latitude, I18n.t('latitude') %> <br />
-#					 <%= f.number_field  :location.latitude %> 
-#					 <%= f.label :location.longitude, I18n.t('longitude') %> <br />
-#					 <%= f.number_field  :location.longitude %>     												
+                     :insert_before => "[data-hook='admin_product_form_right']",
+                     :partial => "spree/admin/products/location",                     				                                  
+					 )            
+					 
