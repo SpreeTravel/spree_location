cb0b230e2d2015b63030bc4a9b547012ba01e1f9
snc <snc@jaf.com>
2013-05-29 22:48:34 -0500

    commend method index in product_decorator

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,11 +1,11 @@
 Spree::Product.class_eval do
   has_one :location, :as => :locationable
     
-  define_index do
-	has location.locationable_id, :as => :locationable_id
-	has "RADIANS(locations.lat)", :as => :latitude, :type => :float
-	has "RADIANS(locations.lng)", :as => :longitude, :type => :float
-  end
+#  define_index do
+#	has location.locationable_id, :as => :locationable_id
+#	has "RADIANS(locations.lat)", :as => :latitude, :type => :float
+#	has "RADIANS(locations.lng)", :as => :longitude, :type => :float
+#  end
   
   def locationable?
 	location.latitude.present? and map.longitude.present? and
