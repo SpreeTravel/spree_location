3ef64e7c94f1ebd3614f1bc9befea0a354f18994
snc <snc@jaf.com>
2013-05-29 23:46:47 -0500

    commend redefine admin_decorator

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -2,5 +2,8 @@ Deface::Override.new(:virtual_path => "spree/backend/app/views/spree/admin/_form
                      :name => "add_latitude_and_longitude",
                      :insert_bottom => "[data-hook='admin_product_form_right']",
                      :text => "<%= f.label :latitude, I18n.t('latitude') %> <br />
-                               <%= f.number_field  I18n.t(latitude) %> ",
-                     :disabled => false)
+                               <%= f.number_field  I18n.t(latitude) %> 
+                               <%= f.label :longitude, I18n.t('longitude') %> <br />
+                               <%= f.number_field  I18n.t(longitude) %> "#,
+                     #:disabled => false
+                     )
