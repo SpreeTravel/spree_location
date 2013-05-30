505ff9f4c110557571f2571bc9977194db6b828c
snc <snc@jaf.com>
2013-05-30 08:17:47 -0500

    modified virtual_path admin_decorator

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -1,9 +1,10 @@
-Deface::Override.new(:virtual_path => "spree/backend/app/views/spree/admin/_form",
+Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_latitude_and_longitude",
-                     :insert_bottom => "[data-hook='admin_product_form_right']",
-                     :text => "<%= f.label :latitude, I18n.t('latitude') %> <br />
-                               <%= f.number_field  I18n.t(latitude) %> 
-                               <%= f.label :longitude, I18n.t('longitude') %> <br />
-                               <%= f.number_field  I18n.t(longitude) %> "#,
-                     #:disabled => false
-                     )
+                     :insert_after => "[data-hook='admin_product_form_right']",
+                     :text => " 
+						<%= f.label :latitude, I18n.t('latitude') %> <br />
+						<%= f.number_field  I18n.t(latitude) %> 
+						<%= f.label :longitude, I18n.t('longitude') %> <br />
+						<%= f.number_field  I18n.t(longitude) %>                      
+                     ")
+                                          
