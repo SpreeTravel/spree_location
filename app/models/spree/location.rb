9d228574cfe6b423ab813157eed7078420bcd6b7
snc <snc@jaf.com>
2013-05-30 11:09:44 -0500

    coment admim_decotator temp

--- app/models/spree/location.rb
+++ app/models/spree/location.rb
@@ -1,5 +1,6 @@
 class Spree::Location < ActiveRecord::Base
   belongs_to :locationable, :polymorphic => true
-  attr_accessible  :locationable, :latitude, :longitude
+  attr_accessible  :locationable, :latitude, :longitude, :locationable_type, :locationable_id
+  
   validates_presence_of :latitude, :longitude, :locationable
 end
--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -1,10 +1,12 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_latitude_and_longitude",
                      :insert_after => "[data-hook='admin_product_form_right']",
-                     :text => " 
-						<%= f.label :latitude, I18n.t('latitude') %> <br />
-						<%= f.number_field  I18n.t(latitude) %> 
-						<%= f.label :longitude, I18n.t('longitude') %> <br />
-						<%= f.number_field  I18n.t(longitude) %>                      
-                     ")
-                                          
+                     :text => "
+						<p>Add Geographic coordinate</p>												
+					 "						
+					 )                                                               
+#				     <%= f.label :location.latitude, I18n.t('latitude') %> <br />
+#					 <%= f.label :latitude, I18n.t('latitude') %> <br />
+#					 <%= f.number_field  :location.latitude %> 
+#					 <%= f.label :location.longitude, I18n.t('longitude') %> <br />
+#					 <%= f.number_field  :location.longitude %>     												
