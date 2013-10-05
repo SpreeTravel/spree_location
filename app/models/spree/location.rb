514d0ef4103d6de7682f22a72840e0d2617d173b
Pedro Quintero Rojas <pqr@uh.cu>
2013-10-04 22:07:27 -0400

    gemspec + gemfile

--- app/models/spree/location.rb
+++ app/models/spree/location.rb
@@ -1,6 +1,5 @@
 class Spree::Location < ActiveRecord::Base
   belongs_to :locatable, :polymorphic => true
-  attr_accessible :latitude, :longitude
 
   # TODO validades :latitude, :longitude
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,4 @@
 Spree::Product.class_eval do
   has_one :location, :as => :locatable
   accepts_nested_attributes_for :location, :allow_destroy => true
-  attr_accessible :location, :location_attributes
 end
