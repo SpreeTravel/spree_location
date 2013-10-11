bc23a354c43e560b31e1b64e03d84178ccf48e46
Pedro Quintero Rojas <pqr@uh.cu>
2013-10-11 12:10:46 -0400

    bug fixing

--- app/overrides/add_location_to_admin_product_form.rb
+++ app/overrides/add_location_to_admin_product_form.rb
@@ -2,5 +2,6 @@ Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_location_to_admin_product_form",
                      :insert_top => "[data-hook='admin_product_form_right']",
                      :partial => "spree/admin/products/location",
-                     :disabled => false
+#                     :original => 'ed8bf0c8913c2515bcbdab6e29a62d0ddb8e8d76',
+#                     :disabled => false
 					 )
