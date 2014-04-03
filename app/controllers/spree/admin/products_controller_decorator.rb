dccf1a2c348c7a8b10019cdff3b653a15441909f
Alejandro Dustet <aledustet@gmail.com>
2014-04-03 19:04:47 -0400

    Fixed bug that kept the location after the deletion of a product

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -3,6 +3,7 @@ Spree::Admin::ProductsController.class_eval do
   before_filter :set_location, :only => :update
   before_filter :initialize_location, :only => :edit
   after_filter :fix_db, :only => :update
+  before_filter :delete_location, :only => :destroy
 
   protected
     def set_location
@@ -26,5 +27,10 @@ Spree::Admin::ProductsController.class_eval do
 
     def initialize_location
       @product.location = @product.location || Spree::Location.create(:locatable_id => @product.id, :locatable_type => 'Spree::Product', :latitude => "0.0", :longitude => "0.0")
+    end
+
+    def delete_location
+      loc = Spree::Location.find_by_locatable_id(@product.id)
+      loc.destroy()
     end
 end
