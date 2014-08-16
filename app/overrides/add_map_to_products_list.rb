cb18a5deb14b2fea2c82cd663b15f6ae69496a36
Pedro Quintero Rojas <pqr@uh.cu>
2014-08-15 23:44:01 -0400

    changing position of map in index

--- app/overrides/add_map_to_products_list.rb
+++ app/overrides/add_map_to_products_list.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => "spree/products/index",
   :name => "add_map_to_products_list",
-  :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+  :insert_after => '[data-hook="homepage_sidebar_navigation"]',
   :partial => "spree/products/products_map",
   :disabled => false
 )
