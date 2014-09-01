9b724328518d9b41c940bc604f65b1da1cc15649
Pedro Quintero Rojas <pqr@uh.cu>
2014-09-01 18:59:05 -0400

    setting default location

--- app/models/spree/location.rb
+++ app/models/spree/location.rb
@@ -2,4 +2,12 @@ class Spree::Location < ActiveRecord::Base
   belongs_to :locatable, :polymorphic => true
   validates :latitude, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }
   validates :longitude, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }
+
+  def self.create_location_for(product)
+    l = Spree::Location.new
+    l.longitude = 0.0
+    l.latitude = 0.0
+    l.locatable = product
+    l.save
+  end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,12 @@
 Spree::Product.class_eval do
   has_one :location, :as => :locatable, class_name: "Spree::Location"
   accepts_nested_attributes_for :location, :allow_destroy => true
+
+  after_save :set_default_location
+
+  def set_default_location
+    unless self.location
+      Spree::Location.create_location_for(self)
+    end
+  end
 end
